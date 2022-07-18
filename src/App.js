baa2427e30fbd9d5e503ed8e1f58c39ce4131c49
dungla <anhdungxd21@gmail.com>
2022-07-18 13:09:07 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Todo from './components/Todo';
 import React from "react";
+import { Route , Switch} from 'react-router-dom';
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Explore the full React course'/>
+      <MainNavigation />
+      <Switch>
+        <Route path={'/'} exact><AllMeetups /></Route>
+        <Route path={'/new-meetup'}><NewMeetup /></Route>
+        <Route path={'/favorites'}><Favorites /></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewMeetup() {
+    return <div>New Meetup Page</div>;
+}
+
+export default NewMeetup;
